b31118b4980f4bea5fc602d76e9daecafba0815f
Louis <louis.masson@palissy.fr>
2019-01-30 20:14:59 +0100

    :sparkles: Add control of FPS

--- inc/initialize.js
+++ inc/initialize.js
@@ -1,11 +1,23 @@
 "use strict";
 
 let canvas;
+let fpsCursor;
+let fpsContainer;
 
 window.onload = function () {
 	canvas = document.querySelector("canvas");
 	setTargetContext(canvas.getContext("2d"));
 	
+	fpsContainer = document.getElementById("fpsContainer");
+
+	fpsCursor = document.querySelector("input");
+	fpsCursor.addEventListener("mousemove", ()=>wantedFPS = fpsCursor.value)
+	fpsCursor.value = wantedFPS = 35;
+	
 	start();
+}
+
+function updateFPS() {
+	wantedFPS = fpsCursor.value;
 }
 
