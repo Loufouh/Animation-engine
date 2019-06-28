46aee0784a95a5a55dbf6eb61d87b259603d8c76
Louis Masson <louis.masson@palissy.fr>
2019-06-28 18:32:16 +0200

    :art: Move fps control to src/sketch.js

--- src/sketch.js
+++ src/sketch.js
@@ -1,22 +1,21 @@
 "use strict"
 
+let fpsCursor;
+
 let dist = 0;
 let radiusGoal = 100;
 let currentRadius = 200;
 
 function setup() {
+	fpsCursor = document.querySelector("input");
 
+	fpsCursor.addEventListener("mousemove", ()=> { wantedFPS = fpsCursor.value } );
+	fpsCursor.value = wantedFPS = 35;
 }
 
 function loop() {
 	background( new Color(200, 100, 10, 1, ColorType.HSL) );
-
-	fill(new Color(255));
-
-	let x = currentRadius*Math.cos(dist*Math.PI/30);
-	let y = currentRadius*Math.sin(dist*Math.PI/30);
-
-	circle(canvas.width/2 + x, canvas.height/2 + y, 50);	
+	drawCircle();
 
 	if(dist >= 360)
 		dist = 0;
@@ -26,5 +25,18 @@ function loop() {
 
 	currentRadius += (currentRadius < radiusGoal)? 1 : -1;
 	dist++;
+}
+
+function drawCircle() {
+	fill(new Color(255));
+
+	let x = currentRadius*Math.cos(dist*Math.PI/30);
+	let y = currentRadius*Math.sin(dist*Math.PI/30);
+
+	circle(canvas.width/2 + x, canvas.height/2 + y, 50);	
+}
+
+function updateFPS() {
+	wantedFPS = fpsCursor.value;
 }
 
