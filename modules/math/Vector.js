303ef0d7e38743dd26497bbf602a09f0b4c8b0ba
Louis Masson <louis.masson.contact@gmail.com>
2020-03-18 11:27:23 +0100

    :arrow_up: Upgrade MathJs

--- modules/math/Vector.js
+++ modules/math/Vector.js
@@ -6,18 +6,46 @@ class Vector {
 		this.y = y;
 	}
 
-	static add(v, w) {
-		return new Vector(v.x + w.x, v.y + v.y);
+	add(v) {
+		let w = Vector.add(this, v);
+
+		this.x = w.x;
+		this.y = w.y;
 	}
 
-	static substract(v, w) {
+	subtract(v) {
+		let w = Vector.subtract(this, v);
+
+		this.x = w.x;
+		this.y = w.y;
+	}
+
+	multiply(v) {
+		let w = Vector.multiply(this, v);
+
+		this.x = w.x;
+		this.y = w.y;
+	}
+
+	divide(v) {
+		let w = Vector.divide(this, v);
+
+		this.x = w.x;
+		this.y = w.y;
+	}
+
+	static add(v, w) {
+		return new Vector(v.x + w.x, v.y + w.y);
+	}
+
+	static subtract(v, w) {
 		return new Vector(v.x - w.x, v.y - w.y);
 	}
 
 	static multiply(v, value) {
 		return new Vector(v.x*value, v.y*value);
 	}
-	
+
 	static divide(v, value) {
 		return new Vector(v.x/value, v.y/value);
 	}
